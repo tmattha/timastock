0f5f6094015280533716eaa9209511b986e0da03
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2024-04-13 09:00:23 +0200

    Extended and optimized universe gathering.

--- src/fmp/universe.py
+++ src/fmp/universe.py
@@ -1,24 +1,45 @@
+from concurrent import futures
+import random
+import time
+from urllib.error import HTTPError
 import pandas as pd
-from . import financials, pricing, company
+import tqdm
+from . import financials, pricing, company, global_vars
+
+def _get_universe_entry(symbol: str, start: str=None, end: str=None) -> dict:
+    symbol_info = {}
+    retry = True
+    while retry:
+        try:
+            if "incomeStatement"    not in symbol_info:
+                symbol_info["incomeStatement"] = financials.income_statement(symbol)
+            if "balanceSheet"       not in symbol_info:
+                symbol_info["balanceSheet"] = financials.balance_sheet(symbol)
+            if "cashflowStatement"  not in symbol_info:
+                symbol_info["cashflowStatement"] = financials.cashflow_statement(symbol)
+            if "metrics"            not in symbol_info:
+                symbol_info["metrics"] = financials.key_metrics(symbol)
+            if "prices"             not in symbol_info:
+                symbol_info["prices"] = pricing.historical_prices(symbol, start=start, end=end)
+            if "marketCap"          not in symbol_info:
+                symbol_info["marketCap"] = pricing.market_cap(symbol, start=start, end=end)
+            if "profile"            not in symbol_info:
+                symbol_info["profile"] = company.company_profile(symbol)
+            if "executives"         not in symbol_info:
+                symbol_info["executives"] = company.executives(symbol)
+            return {symbol: symbol_info}
+        except HTTPError as err:
+            time.sleep(random.randrange(60))
+            retry = True
+        except Exception as err:
+            print(f"Error on symbol {symbol}: {err}")
+            return None
 
 def get_universe(symbols: list, start: str=None, end: str=None) -> list[dict]:
     universe = {}
-    n = len(symbols)
-    for (idx, symbol) in enumerate(symbols):
-        if idx % 20 == 0 or idx == len(symbols) - 1:
-            print(f"Constructing Universe ::: {idx:4d} / {n:4d} :::")
-        try:
-            symbol_info = {}
-            symbol_info["incomeStatement"] = financials.income_statement(symbol)
-            symbol_info["balanceSheet"] = financials.balance_sheet(symbol)
-            symbol_info["cashflowStatement"] = financials.cashflow_statement(symbol)
-            symbol_info["metrics"] = financials.key_metrics(symbol)
-            symbol_info["prices"] = pricing.historical_prices(symbol, start=start, end=end)
-            symbol_info["marketCap"] = pricing.market_cap(symbol, start=start, end=end)
-            symbol_info["profile"] = company.company_profile(symbol)
-            symbol_info["executives"] = company.executives(symbol)
-            universe[symbol] = symbol_info
-        except Exception as err:
-            print(f"Error on symbol {symbol}: {err}")
+    with futures.ThreadPoolExecutor(8) as executor:
+        runner = executor.map(lambda s: _get_universe_entry(s, start, end), symbols)
+        for stat in tqdm.tqdm(runner, "Constructing Universe", len(symbols)):
+            if stat is not None: universe.update(stat)
     print(f"Successfully exported {len(universe)} of {len(symbols)} data sets.")
     return universe
